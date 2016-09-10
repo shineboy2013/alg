ae1a3896af9ac987121736aaf6e6c643906d0783
shineboy2013 <cenjiajie@gmail.com>
2016-09-10 10:15:05 -0400

    add pinyin

--- basic/SelectionSort.java
+++ basic/SelectionSort.java
@@ -1,4 +1,14 @@
-	//pick the min and swap to its pos
+	/*
+	 Problem: 		selection sort,xuanzepaixu
+	 Alg/struture:	
+	 Idea:			pick the min and swap to its pos
+	 Example:
+	 [3,1,2,8,6,4] min=1, swap 1,3 
+	 [1,3,2,8,6,4] min=2, swap 2,3
+	 [1,2,3,8,6,4] min=4, swap 4,8
+	 [1,2,3,4,6,8] 
+	 Time, Space:		O(n^2), O(1)		
+   */
 	public void selectionSort(int[] a){
 		for(int i=0;i<a.length;i++){
 			int min = i;
